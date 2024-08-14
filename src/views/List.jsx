26023d4f89d489777b4d77e3f5c1296d5d1f8d45
Falak Zahra <falak.zahra16@gmail.com>
2024-08-14 10:42:17 -0600

    Render name of each item in a list

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,17 +1,16 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const dataPresent = data.length !== 0;
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
-				{/**
-				 * TODO: write some JavaScript that renders the `data` array
-				 * using the `ListItem` component that's imported at the top
-				 * of this file.
-				 */}
+				{dataPresent
+					? data.map((item) => <ListItem key={item.id} name={item.name} />)
+					: null}
 			</ul>
 		</>
 	);
