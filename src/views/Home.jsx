879fdefd7d7855454e9bbf2afbeace2f0ed62284
Falak Zahra <falak.zahra16@gmail.com>
2024-08-14 10:33:01 -0600

    Render name of each shopping list a user has access to

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,24 @@
 import './Home.css';
+import { SingleList } from '../components';
 
 export function Home({ data, setListPath }) {
+	const dataPresent = data.length !== 0;
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
 			<ul>
-				{/**
-				 * TODO: write some JavaScript that renders the `lists` array
-				 * so we can see which lists the user has access to.
-				 */}
+				{dataPresent
+					? data.map((list, index) => (
+							<SingleList
+								key={index}
+								name={list.name}
+								path={list.path}
+								setListPath={setListPath}
+							/>
+						))
+					: null}
 			</ul>
 		</div>
 	);
