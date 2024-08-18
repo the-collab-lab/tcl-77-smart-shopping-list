136cf25a6846312cd7206497e0bd583dd386a821
Falak Zahra <falak.zahra16@gmail.com>
2024-08-18 15:12:37 -0600

    Use more contextual variable names, and add inline logical && operator

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,23 +2,22 @@ import './Home.css';
 import { SingleList } from '../components';
 
 export function Home({ data, setListPath }) {
-	const dataPresent = data.length !== 0;
+	const hasList = data.length !== 0;
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
 			<ul>
-				{dataPresent
-					? data.map((list, index) => (
-							<SingleList
-								key={index}
-								name={list.name}
-								path={list.path}
-								setListPath={setListPath}
-							/>
-						))
-					: null}
+				{hasList &&
+					data.map((list, index) => (
+						<SingleList
+							key={index}
+							name={list.name}
+							path={list.path}
+							setListPath={setListPath}
+						/>
+					))}
 			</ul>
 		</div>
 	);
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,16 +1,15 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
-	const dataPresent = data.length !== 0;
+	const hasItem = data.length !== 0;
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
-				{dataPresent
-					? data.map((item) => <ListItem key={item.id} name={item.name} />)
-					: null}
+				{hasItem &&
+					data.map((item) => <ListItem key={item.id} name={item.name} />)}
 			</ul>
 		</>
 	);
