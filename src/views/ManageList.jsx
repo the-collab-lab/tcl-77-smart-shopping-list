c8bcd2f111f8cf1d3e1ddada27bed345bcdfbd6f
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-08-19 19:18:06 -0400

    adding a form layout to manage list

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,7 +1,30 @@
 export function ManageList() {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('hi');
+	};
+
 	return (
-		<p>
-			Hello from the <code>/manage-list</code> page!
-		</p>
+		<div>
+			<p>
+				Hello from the <code>/manage-list</code> page!
+			</p>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="item">Item Name</label>
+				<input id="item" type="text" />
+				<br />
+				{/* radio inputs for how soon to buy. */}
+				<input type="radio" id="soon" name="when-to-buy" value="7" />
+				<label htmlFor="soon">Soon</label>
+				<br />
+				<input type="radio" id="kind-of-soon" name="when-to-buy" value="14" />
+				<label htmlFor="kind-of-soon">Kind of soon</label>
+				<br />
+				<input type="radio" id="not-soon" name="when-to-buy" value="30" />
+				<label htmlFor="not-soon">Not soon</label>
+				<br />
+				<button type="submit">Submit Item</button>
+			</form>
+		</div>
 	);
 }
