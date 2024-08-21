61675261f47bcf22baf9bf7f35410c1fe9e8638f
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-08-21 13:51:56 -0400

    cleaning up form tags and adding select possiblity

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -6,8 +6,14 @@ export function ManageList() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		addItem({ itemName: item });
-		console.log('hi');
+		const itemInfoForm = new FormData(e.target);
+
+		console.log({
+			itemName: itemInfoForm.get('item'),
+			daysUntilNextPurchaseSelect: itemInfoForm.get('when-to-buy-select'),
+			daysUntilNextPurchaseRadio: itemInfoForm.get('when-to-buy-radio'),
+		});
+		// addItem({ itemName: itemInfoForm.get("item"), daysUntilNextPurchase: itemInfoForm.get("when-to-buy")});
 		notify();
 	};
 
@@ -17,19 +23,49 @@ export function ManageList() {
 				Hello from the <code>/manage-list</code> page!
 			</p>
 			<form onSubmit={handleSubmit}>
-				<label htmlFor="item">Item </label>
-				<input id="item" type="text" />
+				<label>
+					Item: <input id="item" type="text" name="item" required />
+				</label>
 				<br />
-				{/* radio inputs for how soon to buy. */}
-				<input type="radio" id="soon" name="when-to-buy" value="7" />
-				<label htmlFor="soon">Soon</label>
-				<br />
-				<input type="radio" id="kind-of-soon" name="when-to-buy" value="14" />
-				<label htmlFor="kind-of-soon">Kind of soon</label>
-				<br />
-				<input type="radio" id="not-soon" name="when-to-buy" value="30" />
-				<label htmlFor="not-soon">Not soon</label>
-				<br />
+
+				<label htmlFor="when-to-buy">
+					When to buy:{' '}
+					<select id="when-to-buy" name="when-to-buy-select" required>
+						<option value="" disabled>
+							Select when to buy next
+						</option>
+						<option value="7">Soon (7 days)</option>
+						<option value="14">Kind of Soon (14 days)</option>
+						<option value="30">Not Soon (30 days)</option>
+					</select>
+				</label>
+				<p>
+					When to buy: <br />
+					<label htmlFor="soon">
+						<input type="radio" id="soon" name="when-to-buy-radio" value="7" />
+						Soon
+					</label>
+					<br />
+					<label htmlFor="kind-of-soon">
+						<input
+							type="radio"
+							id="kind-of-soon"
+							name="when-to-buy-radio"
+							value="14"
+						/>
+						Kind of soon
+					</label>
+					<br />
+					<label htmlFor="not-soon">
+						<input
+							type="radio"
+							id="not-soon"
+							name="when-to-buy-radio"
+							value="30"
+						/>
+						Not soon
+					</label>
+				</p>
 				<button type="submit">Submit Item</button>
 			</form>
 			<Toaster />
