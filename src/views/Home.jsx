85fe32c2f53bf20cf80c889e4ff806f82cccf098
Falak Zahra <falak.zahra16@gmail.com>
2024-08-22 17:16:18 -0600

    Add new list and render it on Home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,26 @@
 import './Home.css';
 import { SingleList } from '../components';
+import { useState } from 'react';
+import { createList, useAuth } from '../api';
 
 export function Home({ data, setListPath }) {
 	const hasList = data.length !== 0;
+
+	const { user } = useAuth();
+	const userId = user?.uid;
+	const userEmail = user?.email;
+
+	const [inputValue, setInputValue] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createList(userId, userEmail, inputValue);
+		setInputValue('');
+	};
+
+	const handleChange = (e) => {
+		setInputValue(e.target.value);
+	};
 	return (
 		<div className="Home">
 			<p>
@@ -19,6 +37,15 @@ export function Home({ data, setListPath }) {
 						/>
 					))}
 			</ul>
+
+			<form onSubmit={handleSubmit}>
+				<h3>Create New Shopping List</h3>
+				<label htmlFor="newListName">Name Your List</label>
+				<br />
+				<input type="text" value={inputValue} onChange={handleChange} />
+				<br />
+				<button>Create List</button>
+			</form>
 		</div>
 	);
 }
