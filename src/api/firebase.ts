8183e2dca7c564a66efa865891ef65cf31f1c94e
Maha <maha.ahmed4peace@gmail.com>
2024-08-31 20:53:40 -0400

    fixed backend error message if invited user does not exist

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -212,7 +212,9 @@ export async function shareList(
 	const recipientDoc = await getDoc(doc(usersCollectionRef, recipientEmail));
 	// If the recipient user doesn't exist, we can't share the list.
 	if (!recipientDoc.exists()) {
-		throw new Error("Recipient does not exist!");
+		throw new Error(
+			"Unable to share list. Please verify correct email address.",
+		);
 	}
 	// Add the list to the recipient user's sharedLists array.
 	const listDocumentRef = doc(db, listPath);
