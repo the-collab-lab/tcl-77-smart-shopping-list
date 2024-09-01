8d403d91a29533f52f0757deebe03a39efea5fb4
Falak Zahra <falak.zahra16@gmail.com>
2024-08-31 23:24:02 -0600

    Remove unfilteredListItems from the dependency array for useMemo

--- src/views/List.tsx
+++ src/views/List.tsx
@@ -14,7 +14,7 @@ export function List({ data: unfilteredListItems }: Props) {
 		return unfilteredListItems.filter((item) =>
 			item.name.toLowerCase().includes(searchTerm.toLowerCase()),
 		);
-	}, [searchTerm, unfilteredListItems]);
+	}, [searchTerm]);
 
 	return (
 		<>
