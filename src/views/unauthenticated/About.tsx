78c37163f04eb4a0e86deb7b31abd81bd72fb038
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-09-24 23:55:40 -0400

    mod: a text version/skeleton of the about page of the application

--- src/views/unauthenticated/About.tsx
+++ src/views/unauthenticated/About.tsx
@@ -3,17 +3,54 @@ import React from "react";
 export function About() {
 	return (
 		<div>
+			<h1>ABOUT APPLICATION NAME</h1>
 			<section>
-				<h1>How it all works</h1>
+				<h2>How works</h2>
+				<ul>
+					<li>
+						Create a list for different stores or different grouping of items.
+					</li>
+					<li>Select a list that you&apos;d like to add items on.</li>
+					<li>
+						Open the list manager, to start adding items and choosing when you
+						will need to restock next.
+					</li>
+					<li>
+						Now that your new list has items you can start checking off items as
+						you shop!
+						<ul>
+							<li>
+								Each time an item is marked purchase the application evaluates
+								your shopping habits! Supporting you by adjusting your next
+								purchase predictions base on when your previous shopping
+								history!
+							</li>
+						</ul>
+					</li>
+					<li>
+						If someone else needs to be let into to the shopping time you can
+						easily share specific lists with with in the list manager so they
+						can see and mark items as purchased too!
+					</li>
+				</ul>
 			</section>
 			<section>
 				<h2>Creators</h2>
 				<div>
-					<p></p>
+					<p>Maha Ahmed</p>
+				</div>
+				<div>
+					<p>Brianna Bland</p>
+				</div>
+				<div>
+					<p>Falak Zahra</p>
+				</div>
+				<div>
+					<p>Ross Clettenberg</p>
 				</div>
 			</section>
 			<section>
-				<h2>Thank you to</h2>
+				<h2>Thank you</h2>
 				<p>Mentors: Alex, Aditya, Tanner</p>
 				<p>The entire The Collab Lab.</p>
 			</section>
