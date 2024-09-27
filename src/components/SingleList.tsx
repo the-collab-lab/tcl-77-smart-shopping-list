caed9f5a8ec1a9ddf77e5d5eecb7f9c21c7656a0
Maha <maha.ahmed4peace@gmail.com>
2024-09-26 22:41:32 -0400

    refactored SingleList component to navigate to directly navigate to specified list

--- src/components/SingleList.tsx
+++ src/components/SingleList.tsx
@@ -1,17 +1,17 @@
 import "./SingleList.css";
 import * as api from "../api/firebase";
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 interface Props extends Pick<api.List, "name" | "path"> {
 	setListPath: (path: string) => void;
 }
 
 export function SingleList({ name, path, setListPath }: Props) {
-	//const navigate = useNavigate();
+	const navigate = useNavigate();
 
 	function handleClick() {
 		setListPath(path);
-		// navigate(`/list/${path}`);
+		navigate(`/list/${name}`);
 	}
 
 	return (
