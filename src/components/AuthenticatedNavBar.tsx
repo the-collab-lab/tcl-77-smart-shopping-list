7655b5711a2c8c5b5e7dc7c10c547583364a8c4c
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-09-28 05:13:49 -0400

    fix:  update navbar to use Nav.Link `react-bootstrap` components

--- src/components/AuthenticatedNavBar.tsx
+++ src/components/AuthenticatedNavBar.tsx
@@ -3,27 +3,34 @@ import { NavLink } from "react-router-dom";
 import { SignOutButton } from "../api";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 import "./AuthenticatedNavBar.css";
 
 export function AuthenticatedNavBar() {
 	return (
-		<Navbar className="Nav">
-			<Container className="Nav-container">
+		<Navbar expand="lg">
+			<Container className="d-flex justify-content-around">
 				<SignOutButton />
-				<NavLink to="/" className="Nav-link" aria-label="Home">
+				<Nav.Link as={NavLink} to="/" className="Nav-link" aria-label="Home">
 					Home
-				</NavLink>
-				<NavLink to="/list" className="Nav-link" aria-label="List">
+				</Nav.Link>
+				<Nav.Link
+					as={NavLink}
+					to="/list"
+					className="Nav-link"
+					aria-label="List"
+				>
 					List
-				</NavLink>
-				<NavLink
+				</Nav.Link>
+				<Nav.Link
+					as={NavLink}
 					to="/manage-list"
 					className="Nav-link"
 					aria-label="Manage List"
 				>
 					Manage List
-				</NavLink>
+				</Nav.Link>
 			</Container>
 		</Navbar>
 	);
