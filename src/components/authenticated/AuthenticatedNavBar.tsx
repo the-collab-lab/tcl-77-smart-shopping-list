93c2ca25905fa1a465a400e9799f102c7ccbe776
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-10-04 03:32:51 -0400

    fix: there was a duplication in the AuthenticatedNavBar, made sure the one the homepage is using has the scss styling and made sure the unauthenticated used it as well

--- src/components/authenticated/AuthenticatedNavBar.tsx
+++ src/components/authenticated/AuthenticatedNavBar.tsx
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 
-import "../NavBar.css";
+import "../NavBar.scss";
 
 export function AuthenticatedNavBar() {
 	return (
