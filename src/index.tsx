5d6259058d37b1ae16e9361ab6729e119ed54416
Falak Zahra <falak.zahra16@gmail.com>
2024-10-03 19:10:47 -0600

    Define colours and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.scss";
+// import "./index.scss";
+import "./styles/global.scss";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
