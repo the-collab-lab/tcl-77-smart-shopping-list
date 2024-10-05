eefc30f67844329b6c9730014f47c883bc1d82c7
Maha <maha.ahmed4peace@gmail.com>
2024-10-05 00:12:23 -0400

    merging: mergin main branch and replacing old PR changes back

--- src/components/SingleList.tsx
+++ src/components/SingleList.tsx
@@ -1,11 +1,17 @@
 import Button from "react-bootstrap/Button";
 import "./SingleList.scss";
+import * as api from "../api/firebase";
 import { useNavigate } from "react-router-dom";
 
-export function SingleList({ name }: { name: string }) {
+interface Props extends Pick<api.List, "name" | "path"> {
+	setListPath: (path: string) => void;
+}
+
+export function SingleList({ name, path, setListPath }: Props) {
 	const navigate = useNavigate();
 
 	function handleClick() {
+		setListPath(path);
 		setTimeout(() => {
 			navigate(`/list/${name}`);
 		}, 200);
