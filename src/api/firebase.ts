380072ee6285ead8252727549433d0628cb3a2d5
Maha <maha.ahmed4peace@gmail.com>
2024-10-05 18:35:11 -0400

    fix: list data will populate on new lists using the updated listitemmodel type

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -95,7 +95,7 @@ export function useShoppingLists(user: User | null) {
 const ListItemModel = t.type({
 	id: t.string,
 	name: t.string,
-	//itemQuantity: t.number,
+	itemQuantity: t.number,
 	dateLastPurchased: t.union([FirebaseTimestamp, t.null]),
 	dateNextPurchased: FirebaseTimestamp,
 	totalPurchases: t.number,
