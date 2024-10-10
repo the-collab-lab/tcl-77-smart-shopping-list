a724ecb78c751a1858ccded97b4d869751ae0838
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-10-10 00:32:13 -0400

    card title on carousel card

--- src/views/unauthenticated/About.tsx
+++ src/views/unauthenticated/About.tsx
@@ -18,34 +18,7 @@ export function About() {
 	return (
 		<div className="d-flex flex-wrap mx-lg-5 my-3 align-items-center justify-content-center gap-2">
 			<Card className="border border-solid rounded-2 border-dark border-3 bg-primary text-info shadow">
-				<h2>How it works</h2>
-				<ul>
-					{/* <li>
-						Create a list for different stores or different grouping of items.
-					</li> */}
-					{/* <li>Select a list that you&apos;d like to add items on.</li> */}
-					{/* <li>
-						Open the list manager, to start adding items and choosing when you
-						will need to restock next.
-					</li> */}
-					{/* <li>
-						Now that your new list has items you can start checking off items as
-						you shop!
-						<ul>
-							<li>
-								Each time an item is marked purchase the application evaluates
-								your shopping habits! Supporting you by adjusting your next
-								purchase predictions base on when your previous shopping
-								history!
-							</li>
-						</ul>
-					</li> */}
-					{/* <li>
-						If someone else needs to be let into to the shopping time you can
-						easily share specific lists with with in the list manager so they
-						can see and mark items as purchased too!
-					</li> */}
-				</ul>
+				<Card.Title>How it works</Card.Title>
 				<Carousel activeIndex={stepIndex} onSelect={handleStepIndexChoice}>
 					<Carousel.Item>
 						<Image src={SampleImage} fluid />
