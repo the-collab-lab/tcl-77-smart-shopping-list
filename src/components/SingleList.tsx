0f4a1d3a7b38106791f9c820ba8b2f77405bd98a
RossaMania <raclettenberg@gmail.com>
2024-10-09 21:26:39 -0500

    Refactor SingleList component in src/components/SingleList.tsx to add a delete button and handle the deletion of a list. Also, pass the User object as a prop to the component.

--- src/components/SingleList.tsx
+++ src/components/SingleList.tsx
@@ -2,12 +2,14 @@ import Button from "react-bootstrap/Button";
 import "./SingleList.scss";
 import * as api from "../api/firebase";
 import { useNavigate } from "react-router-dom";
+import { deleteList } from "./../api";
 
 interface Props extends Pick<api.List, "name" | "path"> {
 	setListPath: (path: string) => void;
+	User: api.User;
 }
 
-export function SingleList({ name, path, setListPath }: Props) {
+export function SingleList({ name, path, setListPath, User }: Props) {
 	const navigate = useNavigate();
 
 	function handleClick() {
@@ -17,9 +19,18 @@ export function SingleList({ name, path, setListPath }: Props) {
 		}, 200);
 	}
 
+	function handleDelete() {
+		console.log("Delete button clicked!");
+		console.log("Deleting list", name);
+		deleteList(name, User);
+	}
+
 	return (
 		<li className="SingleList">
 			<Button onClick={handleClick}>{name}</Button>
+			<Button variant="danger" onClick={handleDelete}>
+				Delete List
+			</Button>
 		</li>
 	);
 }
